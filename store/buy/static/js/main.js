9fac885b607cce8d4a9f46ba5ebc60317bd05787
Daniel Raitan <raitandaniel@gmail.com>
2023-09-04 17:17:42 +0300

    login complete

--- store/buy/static/js/main.js
+++ store/buy/static/js/main.js
@@ -1,6 +1,6 @@
 function myFunction() {
-    alert("Thank you we will get back to you as soon as we can!");
+    alert("");
 }
 function myFunction2() {
-    alert("your car is now listed for sale thank you!");
+    alert("thank you!");
 }
